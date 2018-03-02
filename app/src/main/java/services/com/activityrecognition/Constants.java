d5c17bf552e2c790dcb3ac4eb8923a802d2dcc60
rana.narin <rana.narin@gmail.com>
2018-03-02 15:27:00 +0530

    commit tested

---
+++ app/src/main/java/services/com/activityrecognition/Constants.java
@@ -0,0 +1,14 @@
+package services.com.activityrecognition;
+
+/**
+ * Created by narinder on 02/03/18.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+
+    public static final int CONFIDENCE = 70;
+}
